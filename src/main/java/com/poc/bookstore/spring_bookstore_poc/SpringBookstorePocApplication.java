833e0159d867ae2336ca24b15011fe99eee601a2
Sandeep Singh Bhandari <sandybhndr05@gmail.com>
2025-01-14 22:48:40 +0800

    Initial Commit

---
+++ src/main/java/com/poc/bookstore/spring_bookstore_poc/SpringBookstorePocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.bookstore.spring_bookstore_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookstorePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookstorePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/bookstore/spring_bookstore_poc/SpringBookstorePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.bookstore.spring_bookstore_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookstorePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
